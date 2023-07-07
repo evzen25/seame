62efdcb7554f42b436ee1cae3fbd4069917a5fa4
evzen25 <121442335+evzen25@users.noreply.github.com>
2023-07-07 16:41:31 +0200

    Add files via upload
    In this model I will use the example car, Opel Astra. We will define additional derived classes for various parts of the Opel Astra car.
    I have added detailed classes for the Opel Astra car. The Wheel class, for example, includes properties such as material and size, 
    while the Brake class includes the brake type. The Transmission class includes the transmission type.
    
    When you run the main() function, it will create an instance of the OpelAstra class (myOpelAstra) and call the printDetails() 
    function to display the detailed information about the Opel Astra, including the engine, wheels, brakes, and transmission.

---
+++ Module02_Exercise_01_Model_your_own_Car.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Base class for car parts
+class Part {
+public:
+    virtual void print() = 0; // Pure virtual function to be overridden by derived classes
+};
+
+// Engine class
+class Engine : public Part {
+public:
+    void print() override {
+        cout << "Engine: Opel Astra Engine\n";
+    }
+};
+
+// Wheel class
+class Wheel : public Part {
+public:
+    Wheel(const string& material, int size) : material_(material), size_(size) {}
+
+    void print() override {
+        cout << "Wheel: Material - " << material_ << ", Size - " << size_ << " inches\n";
+    }
+
+private:
+    string material_;
+    int size_;
+};
+
+// Brake class
+class Brake : public Part {
+public:
+    Brake(const string& type) : type_(type) {}
+
+    void print() override {
+        cout << "Brake: Type - " << type_ << "\n";
+    }
+
+private:
+    string type_;
+};
+
+// Transmission class
+class Transmission : public Part {
+public:
+    Transmission(const string& type) : type_(type) {}
+
+    void print() override {
+        cout << "Transmission: Type - " << type_ << "\n";
+    }
+
+private:
+    string type_;
+};
+
+// OpelAstra class representing the entire car
+class OpelAstra {
+public:
+    OpelAstra() {
+        engine_ = new Engine();
+        wheels_ = new Wheel[4]{
+            Wheel("Alloy", 17),
+            Wheel("Alloy", 17),
+            Wheel("Alloy", 17),
+            Wheel("Alloy", 17)
+        };
+        brakes_ = new Brake[4]{
+            Brake("Disc"),
+            Brake("Disc"),
+            Brake("Disc"),
+            Brake("Disc")
+        };
+        transmission_ = new Transmission("Automatic");
+    }
+
+    void printDetails() {
+        cout << "Opel Astra Details:\n";
+        engine_->print();
+        for (int i = 0; i < 4; ++i) {
+            wheels_[i].print();
+            brakes_[i].print();
+        }
+        transmission_->print();
+    }
+
+    ~OpelAstra() {
+        delete engine_;
+        delete[] wheels_;
+        delete[] brakes_;
+        delete transmission_;
+    }
+
+private:
+    Engine* engine_;
+    Wheel* wheels_;
+    Brake* brakes_;
+    Transmission* transmission_;
+};
+
+int main() {
+    OpelAstra myOpelAstra;
+    myOpelAstra.printDetails();
+
+    return 0;
+}
